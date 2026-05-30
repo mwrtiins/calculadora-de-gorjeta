802f5a3fa29671debf1ad0650bf8aae15682c7c9
ma <mwrtiins@gmail.com>
2026-05-29 23:42:41 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -29,19 +29,30 @@ function receberQuantidadePessoas(evento){
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento){
+function receberPorcentagem(evento){
 
     botoesGorjeta.forEach(botao => {
 
         botao.classList.remove("botao-ativo")
-        
+
         if(botao.value === evento.target.value){
             botao.classList.add("botao-ativo")
         }
     })
+
+    if(evento.target.value !== ""){
+        porcentagem = parseFloat(evento.target.value) / 100
+
+    } else{
+        porcentagem = 0
+    }
         
-    porcentagem = parseFloat(evento.target.value) / 100
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+
+gorjetaInput.addEventListener("input",receberPorcentagem)
